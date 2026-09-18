121d8ed079fef9575947570ee53c7bf533dc1be5
Mohd sahil <saahilansari776@GMAIL.COM>
2026-09-18 10:05:43 +0530

    Time: 3 ms (5.58%) | Memory: 50.7 MB (5.39%) - LeetSync

--- 344-reverse-string/reverse-string.java
+++ 344-reverse-string/reverse-string.java
@@ -5,11 +5,10 @@ class Solution {
         int x = s.length;
         Vector<Character> c = new Vector<>();
 
-        for (int i = x - 1; i >= 0; i--) {
+        for(int  i = x-1;i>=0;i--){
             c.add(s[i]);
         }
-
-        for (int i = 0; i < x; i++) {
+        for(int i = 0;i<x;i++){
             s[i] = c.get(i);
         }
     }
