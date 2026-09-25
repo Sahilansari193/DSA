e26b2dd444962b0ec84775dfddf17bcff9800b80
Mohd sahil <saahilansari776@GMAIL.COM>
2026-09-25 11:30:22 +0530

    Time: 2 ms (91.62%) | Memory: 48 MB (20.28%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,26 +1,19 @@
-
-import java.util.*;
-
 class Solution {
     public void moveZeroes(int[] nums) {
-        Queue<Integer> q = new LinkedList<>();
 
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != 0) {
-                q.add(nums[i]);
+        int j  = 0;
+        for(int i = 0;i<nums.length;i++){
+            if(nums[i] !=0){
+                swap(nums,i,j);
+                j++;
             }
         }
 
-        int i = 0;
-
-        while (!q.isEmpty()) {
-            nums[i] = q.poll();
-            i++;
-        }
-
-        while (i < nums.length) {
-            nums[i] = 0;
-            i++;
-        }
+        
+    }
+    public void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
